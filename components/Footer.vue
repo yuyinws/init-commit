dfd35d704c24c4f80eec14a8c66333cf65d57550
yuyinws <lyc657508366@gmail.com>
2022-09-18 17:39:16 +0800

    chore: link

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,9 +2,9 @@
   <div flex="~ col" fixed bottom-2 text-12px w-full items-center justify-center>
     <div flex gap-x-5px>
       <div>Powered By</div>
-      <a color="#2e67d3" href="">Github GraphQL API</a>
+      <a color="#2e67d3" href="https://docs.github.com/en/graphql">Github GraphQL API</a>
       <div>&</div>
-      <a color="#4daa9f" href="">Netlify</a>
+      <a color="#4daa9f" href="https://netlify.app/">Netlify</a>
     </div>
   </div>
 </template>
