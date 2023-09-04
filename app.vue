2fd7337684fce3d80889aa1d23019ddebf8bd98d
yuyinws <lyc657508366@gmail.com>
2023-09-04 10:46:48 +0800

    chore: app head

--- app.vue
+++ app.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+useHead({
+  title: 'Comm1t',
+  link: [
+    { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
+  ],
+  meta: [
+    { name: 'description', content: 'Find the initial commit of a repository.' },
+  ],
+})
+
+useSeoMeta({
+  title: 'Comm1t',
+  ogTitle: 'Comm1t',
+  description: 'Find the initial commit of a repository.',
+  ogDescription: 'Find the initial commit of a repository.',
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
